805d5edac2c56212c4d8dacbacc9e0369eb85a87
jasmine-chen03 <jc115@wellesley.edu>
2025-10-19 16:26:25 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,45 @@
+import requests
+import urllib
+
+def runQuery(query, start_year=1850, 
+             end_year=1860, corpus=26,
+             smoothing=0):
+
+    # converting a regular string to 
+    # the standard URL format
+    # eg: "geeks for,geeks" will
+    # convert to "geeks%20for%2Cgeeks"
+    query = urllib.parse.quote(query)
+
+    # creating the URL
+    url = 'https://books.google.com/ngrams/json?content=' + query +
+    '&year_start=' + str(start_year) + '&year_end=' +
+    str(end_year) + '&corpus=' + str(corpus) + '&smoothing=' +
+    str(smoothing) + ''
+
+    # requesting data from the above url
+    response = requests.get(url)
+
+    # extracting the json data from the response we got
+    output = response.json()
+
+    # creating a list to store the ngram data
+    return_data = []
+
+    if len(output) == 0:
+        # if no data returned from site,
+        # print the following statement
+        return "No data available for this Ngram."
+    else:
+        # if data returned from site,
+        # store the data in return_data list
+        for num in range(len(output)):
+          
+              # getting the name
+            return_data.append((output[num]['ngram'], 
+                                
+                                # getting ngram data
+                                output[num]['timeseries']) 
+                               )
+
+    return return_data
